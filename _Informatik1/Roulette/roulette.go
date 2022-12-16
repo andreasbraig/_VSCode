4c4b9cd24198b78fb28201252dc1c0b5d8e9dbb8
andreasbraig <jpotze22@gmail.com>
2022-12-16 12:38:24 +0100

    Roulette V2

--- _Informatik1/Roulette/roulette.go
+++ _Informatik1/Roulette/roulette.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 func ReadNumber(msg string) int {
@@ -17,19 +16,41 @@ func ReadNumber(msg string) int {
 
 }
 
-func main() {
+// prüft ob der Spieler mit seiner gewählten Zahl gewonnen hat
+// Braucht dazu die gewählte Zahl und die Zahl, die gedreht wurde
+func PlayerWins(playerNumber, wheelNumber int) bool {
 
-	var input int = ReadNumber("Geben sie eine Zahl zwischen 0 und 36 ein: ")
+	if playerNumber == wheelNumber { //Bestimmt ob Player Number und Wheel Number dieselbe sind
+		return true
+	} else {
+		return false
+	}
 
-	//Am Rad drehen
+}
 
-	var result int = rand.Int(36 - 0)
+// Gibt das ergebnis auf die Konsole aus
+// Braucht dazu die gewählte Zahl und die Zahl, die gedreht wurde
+func PrintResult(playerNumber, wheelNumber int) {
 
-	// Auswerten ob spieler gewonnen hat
-
-	if input == result {
-		fmt.Println("Sie haben gewonnen!")
+	if PlayerWins(playerNumber, wheelNumber) { //Prüft ob der wert PlayerWins True oder false ist
+		fmt.Println("Sie haben Gewonnen!")
 	} else {
 		fmt.Println("Sie haben leider verloren.")
 	}
+}
+
+func main() {
+
+	//Spieler Abfragen
+
+	var input int = ReadNumber("Bitte geben sie eine Zahl zwischen 0 und 36 ein: ")
+
+	//Am Rad drehen
+
+	var result int = 13
+
+	// Auswerten ob spieler gewonnen hat
+
+	PrintResult(input, result)
+
 }
