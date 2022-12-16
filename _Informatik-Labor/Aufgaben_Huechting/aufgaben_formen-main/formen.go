5a91bd54c1f9445d19b9407cc750a464659e9ac7
andreasbraig <jpotze22@gmail.com>
2022-12-16 15:10:47 +0100

    Formen Solved!

--- _Informatik-Labor/Aufgaben_Huechting/aufgaben_formen-main/formen.go
+++ _Informatik-Labor/Aufgaben_Huechting/aufgaben_formen-main/formen.go
@@ -1,12 +1,17 @@
 package main
 
+import "fmt"
+
 // Erwartet eine Zahl n und gibt ein Quadrat der Seitenlänge n auf die Konsole aus.
 // Das Quadrat soll mit Hilfe des Zeichens "*" gezeichnet werden.
 func PrintSquare(n int) {
 	// TODO
-
-	
-
+	for i := 1; i <= n; i++ { //Anzahl der Zeilen
+		for j := 0; j < n; j++ { //Anzahl der Sterne Pro Zeile
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
 }
 
 // Erwartet eine Zahl n und gibt ein Quadrat der Seitenlänge n auf die Konsole aus.
@@ -14,12 +19,44 @@ func PrintSquare(n int) {
 // Das Innere soll aus Leerzeichen bestehen.
 func PrintEmptySquare(n int) {
 	// TODO
+	for i := 0; i < n; i++ { // erste Zeile
+		fmt.Print("*")
+	}
+	fmt.Println()               // umbruch
+	for i := 1; i <= n-2; i++ { //Anzahl der Zeilen die hohl sind
+		fmt.Print("*")
+		for j := 0; j < n-2; j++ { //Sterne Pro Zeile
+			fmt.Print(" ")
+		}
+		fmt.Print("*")
+		fmt.Println() //umbruch nach Zeile
+	}
+	for i := 0; i < n; i++ { // erste Zeile
+		fmt.Print("*")
+	}
+	fmt.Println()
 }
 
-// Erwartet eine Zahl n und gibt ein Quadrat der Seitenlänge n auf die Konsole aus.
+// Erwartet eine Zahl  n und gibt ein Quadrat der Seitenlänge n auf die Konsole aus.
 // Die Zeichen für Rand und Inneres werden als Parameter angegeben.
 func PrintCustomSquare(n int, border, fill string) {
 	// TODO
+	for i := 0; i < n; i++ { // erste Zeile
+		fmt.Print(border)
+	}
+	fmt.Println()               // umbruch
+	for i := 1; i <= n-2; i++ { //Anzahl der Zeilen die hohl sind
+		fmt.Print(border)
+		for j := 0; j < n-2; j++ { //Sterne Pro Zeile
+			fmt.Print(fill)
+		}
+		fmt.Print(border)
+		fmt.Println() //umbruch nach Zeile
+	}
+	for i := 0; i < n; i++ { // erste Zeile
+		fmt.Print(border)
+	}
+	fmt.Println()
 }
 
 // Erwartet zwei Zahlen m und n und gibt ein Rechteck
@@ -27,6 +64,13 @@ func PrintCustomSquare(n int, border, fill string) {
 // Das Rechteck soll mit dem Zeichen "*" ausgefüllt sein.
 func PrintRectangle(m, n int) {
 	// TODO
+	for i := 1; i <= n; i++ { //Anzahl der Zeilen
+		for j := 0; j < m; j++ { //Anzahl der Sterne Pro Zeile
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
 }
 
 // Erwartet eine Zahl n und gibt ein Dreieck auf die Konsole aus.
@@ -35,4 +79,11 @@ func PrintRectangle(m, n int) {
 // Das Dreieck soll mit dem Zeichen "*" ausgefüllt sein.
 func PrintTriangle(n int) {
 	// TODO
+	for i := 1; i <= n; i++ { //Anzahl der Zeilen
+		for j := 0; j < n; { //Anzahl der Sterne Pro Zeile
+			fmt.Print("*")
+		}
+		fmt.Println()
+		n = n - 1
+	}
 }
