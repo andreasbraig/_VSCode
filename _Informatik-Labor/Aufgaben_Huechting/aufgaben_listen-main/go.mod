b2af3624e346e4b13ff312b9b7c045a88c9f9237
andreasbraig <jpotze22@gmail.com>
2023-01-09 11:38:52 +0100

    Solved Listen

---
+++ _Informatik-Labor/Aufgaben_Huechting/aufgaben_listen-main/go.mod
@@ -0,0 +1,3 @@
+module github.com/tel22a.inf/aufgaben_listen
+
+go 1.19
---
+++ _testumgebung/Listen-Tester/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func Listen() {
+	l1 := []int{13, 2, 4, 25, 4, 12, 2, -3, 15, 25, 13, 4, 75, 42, 2, 4, 13, 10, 4, 17, 38}
+
+	n := 2
+
+	result := 0
+
+	count := 0
+
+	for _, element := range l1 {
+		if element > n {
+			result++
+		}
+
+		count++
+
+	}
+
+	fmt.Println(len(l1))
+
+	fmt.Println(count, result)
+}
+
+func Calc() bool{
+	var n int = 13 
+	var m int = 13
+
+	return n < m 
+}
+
+func main() {
+
+	fmt.Println(Calc())
+
+
+}
