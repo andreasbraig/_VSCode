5ecf14e8ef7df79cb9d50bb7b59ea7ab12df869b
andreasbraig <jpotze22@gmail.com>
2023-01-13 13:07:42 +0100

    Vorlesung 13.01.2023 Final

--- _Informatik1/Code_Vorlesung/2023-01-13/foo/strings.go
+++ _Informatik1/Code_Vorlesung/2023-01-13/foo/strings.go
@@ -12,17 +12,28 @@ func CreateStringSliceWithOneElement(start string) []string {
 	return []string{start}
 }
 
-//Durchsucht die Liste nach dem Wort W und gibt die Position des ersten Vorkommens zurück 
+//Durchsucht die Liste nach dem Wort W und gibt die Position des ersten Vorkommens zurück
 
 func FindString(list []string, w string) int {
 
-
-	for idx,element := range list {
+	for idx, element := range list {
 		if element == w {
 			return idx
 		}
 	}
 
-	return 0
+	return -1
+
+}
+
+// Erwartet zwei listen und ein wort w. Sucht die Position von w in list 1 und liefert das entsprechende Wort in list 2
+
+func LookupString(list1, list2 []string, w string) string {
+
+	pos := FindString(list1, w)
+	if pos == -1 || pos >= len(list2) {
+		return " "
+	}
+	return list2[pos]
 
 }
--- _Informatik1/Code_Vorlesung/2023-01-13/foo/strings_test.go
+++ _Informatik1/Code_Vorlesung/2023-01-13/foo/strings_test.go
@@ -23,6 +23,24 @@ func ExampleFindString() {
 	// 0
 	// 3
 	// 4
-	// 0
+	// -1
+
+}
+
+func ExampleLookupString() {
+
+	list1 := CreateEmptyStringSlice()
+	list1 = append(list1, "Max Mustermann", "Monika Mustermann")
+	list2 := CreateEmptyStringSlice()
+	list2 = append(list2, "0123-45678", "9876-54321")
+
+	fmt.Println(LookupString(list1, list2, "Max Mustermann"))
+	fmt.Println(LookupString(list1, list2, "Monika Mustermann"))
+	fmt.Println(LookupString(list1, list2, "test"))
+
+	// Output:
+	// 0123-45678
+	// 9876-54321
+	//
 
 }
